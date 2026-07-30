049934f51a76c6be304d05445ac34312881a13a5
fmohamed-a11y <f.mohamed@alustudent.com>
2026-07-30 10:00:31 -0700

    square with private size, no validation yet

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a Square class with a private size attribute"""
+
+
+class Square:
+    """Define a square with a private size attribute"""
+
+    def __init__(self, size):
+        """Sets the size of the square"""
+       self.__size = size
