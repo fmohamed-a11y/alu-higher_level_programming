ebb55912abd5f9438c608b1b50afe814308a8097
fmohamed-a11y <f.mohamed@alustudent.com>
2026-07-31 04:15:01 -0700

    adding my_print method, draw square with # chars

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This module defines a Square class that can print itself"""
+
+
+class Square:
+    """Defines a square that prints itself with # characters"""
+
+    def __init__(self, size=0):
+        """Sets the size using the setter"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieves the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size with type and value validation"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square using # characters"""
+        if self.__size == 0:
+            print("")
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
