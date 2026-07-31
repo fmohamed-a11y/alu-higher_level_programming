a98ada0b1dbcb488969f7da64215aec00782c4d0
fmohamed-a11y <f.mohamed@alustudent.com>
2026-07-31 14:53:07 -0700

    fixing errors 2

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """This script adds arguments to a list and saves it to a JSON file"""
 import sys
+import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-try:
+if os.path.exists("add_item.json"):
     my_list = load_from_json_file("add_item.json")
-except FileNotFoundError:
+else:
     my_list = []
 
 my_list.extend(sys.argv[1:])
