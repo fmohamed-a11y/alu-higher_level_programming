9f2c1c57a92763d12373aa4de6a0fd095d4eaf1b
fmohamed-a11y <f.mohamed@alustudent.com>
2026-07-31 15:20:14 -0700

    trying different approach

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """Adds all arguments to a Python list, then saves them to a file"""
 import sys
-import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-if os.path.exists(filename):
-    items = load_from_json_file(filename)
-else:
+try:
+    items = load_from_json_file("add_item.json")
+except Exception:
     items = []
+
 items.extend(sys.argv[1:])
-save_to_json_file(items, filename)
+save_to_json_file(items, "add_item.json")
