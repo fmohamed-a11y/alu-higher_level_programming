11641f439e11dff1ec8b58eddc4dada0ad721a09
fmohamed-a11y <f.mohamed@alustudent.com>
2026-07-31 05:37:13 -0700

    fixing the output mistake

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -53,7 +53,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width] * self.__height)
-
-    def __repr__(self):
-        """Returns official string representation of the rectangle"""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
