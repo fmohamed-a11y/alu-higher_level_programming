bd8c5335eaf8b798dd1782d6b609776db55343da
fmohamed-a11y <f.mohamed@alustudent.com>
2026-07-31 15:06:23 -0700

    diff approach

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""This module defines a save_to_json_file function"""
+"""This module defines a load_from_json_file function"""
 import json
 
 
-def save_to_json_file(my_obj, filename):
-    """Writes an object to a file using its JSON representation"""
-    with open(filename, "w", encoding="utf-8") as f:
-        json.dump(my_obj, f)
+def load_from_json_file(filename):
+    """Creates a Python object from a JSON file"""
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
