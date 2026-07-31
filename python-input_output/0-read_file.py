03dd75fecb779df6678781de8eb9077ce03a6f2b
fmohamed-a11y <f.mohamed@alustudent.com>
2026-07-31 14:07:54 -0700

    a fun that reads a file and prints it to stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a read_file function"""
+
+
+def read_file(filename=""):
+    """Reads a text file and prints it to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
