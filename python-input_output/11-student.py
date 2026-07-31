01fa65ec82aa145254bd6d0acd996933fb07e562
fmohamed-a11y <f.mohamed@alustudent.com>
2026-07-31 14:39:07 -0700

    reload_from_json replaces studenr attribtes from dicts, like loading from disk

---
+++ python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module defines a Student class with save and reload"""
+
+
+class Student:
+    """Defines a student with first name, last name and age"""
+
+    def __init__(self, first_name, last_name, age):
+        """Sets first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns dict of Student, filtered by attrs if provided"""
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student from a dictionary"""
+        for k, v in json.items():
+            setattr(self, k, v)
