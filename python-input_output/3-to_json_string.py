ba7871c7a9a239b0ec959854f44abd172775c6bb
fmohamed-a11y <f.mohamed@alustudent.com>
2026-07-31 14:16:27 -0700

    a fun that converts a n object to its JSON string representation

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a to_json_string function"""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object as a string"""
+    return json.dumps(my_obj)
