a79bac915e44fd22e3e1e2733d49321834cc8dee
fmohamed-a11y <f.mohamed@alustudent.com>
2026-07-31 14:21:10 -0700

    a func that saves an object to a file an JSON

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a save_to_json_file function"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes an object to a file using its JSON representation"""
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
