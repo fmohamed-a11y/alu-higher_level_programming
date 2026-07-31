f9e69863aa4cf51289880b1b5efc030520a61a60
fmohamed-a11y <f.mohamed@alustudent.com>
2026-07-31 04:06:13 -0700

    typo mistake

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,8 +2,8 @@
 """This module defines a Square class with an area method"""
 
 
-  class Square:
-    """Define a square with a Square class with an are method"""
+class Square:
+    """Defines a square with size validation and area calculation"""
 
     def __init__(self, size=0):
         """Sets the size with type and value validation"""
@@ -11,8 +11,8 @@
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size 
+        self.__size = size
 
     def area(self):
-        """Return the area of the square"""
+        """Returns the area of the square"""
         return self.__size ** 2
