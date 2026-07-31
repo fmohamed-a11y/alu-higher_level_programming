774327f202b08a7d3a1e63c7f74131ddd8b6b382
fmohamed-a11y <f.mohamed@alustudent.com>
2026-07-31 14:51:06 -0700

    fixing errors

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,7 +7,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
     my_list = load_from_json_file("add_item.json")
-except Exception:
+except FileNotFoundError:
     my_list = []
 
 my_list.extend(sys.argv[1:])
