7ce5d2c1295ace7b4e056f7ff20a22e985e26586
fmohamed-a11y <f.mohamed@alustudent.com>
2026-08-01 09:34:01 -0700

    a script that displays a github user id using basic auth

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Display the id of a GitHub user using Basic Authentication."""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    response = requests.get(
+        "https://api.github.com/user",
+        auth=(username, password)
+    )
+    if response.status_code == 200:
+        print(response.json().get("id"))
+    else:
+        print(None)
