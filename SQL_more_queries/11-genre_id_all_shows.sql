e162fc33519fc0f326184334ac93cbd843035557
fmohamed-a11y <f.mohamed@alustudent.com>
2026-08-01 08:11:15 -0700

    fixing the no comment problem

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
- -- Lists all shows with genre id, using a left join to include shows with no genre
+-- Lists all shows with genre id, using a left join to include shows with no genre
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
