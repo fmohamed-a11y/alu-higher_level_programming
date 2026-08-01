ccc616a29e3f1918456841f5b5e4f1a4b6655cf6
fmohamed-a11y <f.mohamed@alustudent.com>
2026-08-01 08:08:16 -0700

    adding more comments

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,6 @@
 -- Lists only shows that have no genre linked
+-- Left join keeps every show, even ones with no match in tv_show_genres
+-- Those unmatched shows get genre_id = NULL, which the WHERE clause filters for
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
