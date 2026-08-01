d52843391165dfc621be1479686fb9f1f8522c7f
fmohamed-a11y <f.mohamed@alustudent.com>
2026-08-01 08:02:03 -0700

    a script that list all of the shows including those without a genre

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+ -- Lists all shows with genre id, using a left join to include shows with no genre
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
