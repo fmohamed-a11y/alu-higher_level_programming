4f94bc27d6363b332f7539b6c60b557c3663a6be
fmohamed-a11y <f.mohamed@alustudent.com>
2026-08-01 09:31:26 -0700

    a script that searches the user api and displays json results

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Send a POST request to the search_user endpoint and display JSON results."""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(url, data={"q": letter})
+
+    try:
+        result = response.json()
+    except ValueError:
+        print("Not a valid JSON")
+    else:
+        if not result:
+            print("No result")
+        else:
+            print("[{}] {}".format(result.get("id"), result.get("name")))
