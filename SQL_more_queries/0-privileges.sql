1358582e0afe6d13463f71012c4179de09720aaf
fmohamed-a11y <f.mohamed@alustudent.com>
2026-08-01 07:29:12 -0700

    a script that create force_name table where name can't be null

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- Revoke the newer dynamic privileges not present in the reference
--- environment, so output matches regardless of MySQL patch version.
 REVOKE AUDIT_ABORT_EXEMPT, AUTHENTICATION_POLICY_ADMIN, FIREWALL_EXEMPT,
     GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN,
     SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN
@@ -9,6 +8,6 @@ REVOKE AUDIT_ABORT_EXEMPT, AUTHENTICATION_POLICY_ADMIN, FIREWALL_EXEMPT,
     SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN
     ON *.* FROM 'user_0d_2'@'localhost';
 
--- Lists all privileges of the users user_0d_1 and user_0d_2
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
