fbe210cd1eaf0733b47287057fdf8ec5f2e03238
fmohamed-a11y <f.mohamed@alustudent.com>
2026-08-01 13:36:57 -0700

    fix filter states to use case sensitive comparison with BINARY

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(
         sys.argv[4])
     cursor.execute(query)
     rows = cursor.fetchall()
