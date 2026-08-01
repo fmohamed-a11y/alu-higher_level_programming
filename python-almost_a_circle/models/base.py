708c41d638e8c0a1167fb65b84998da6b1e4d98c
fmohamed-a11y <f.mohamed@alustudent.com>
2026-08-01 12:15:14 -0700

    adding task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""This module defines the Base class"""
+"""Defines the Base class."""
+
+import json
 
 
 class Base:
-    """Base class that manages the id attribute for all future classes"""
+    """Base class."""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new Base instance
-
-        Args:
-            id: the id to assign, or None to auto-increment
-        """
+        """Initialize a Base instance."""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of a list of dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
