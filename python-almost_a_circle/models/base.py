0dd150cb19d23a5a476ddbe73b3abe9c6356191b
fmohamed-a11y <f.mohamed@alustudent.com>
2026-08-01 11:25:57 -0700

    Base class with auto id, to_json and from_json methods

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
 """This module defines the Base class"""
+import json
 
 
 class Base:
-    """Base class that manages the id attribute for all future classes"""
+    """Base class for managing id attributes"""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new Base instance
-
-        Args:
-            id: the id to assign, or None to auto-increment
-        """
+        """Sets id automatically or uses given id"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns JSON string of a list of dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns list from a JSON string"""
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
