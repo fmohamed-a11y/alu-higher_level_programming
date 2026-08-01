1f5b8f7a00ef29e1a40e59ccdc058eeb33270089
fmohamed-a11y <f.mohamed@alustudent.com>
2026-08-01 07:50:32 -0700

    a script that lists cities of california using subquery no join

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- Lists cities of California using a subquery, no JOIN
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (SELECT id FROM states WHERE name = "California")
+ORDER BY cities.id ASC;
