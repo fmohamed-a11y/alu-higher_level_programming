a9c156b9107439fe99f96fea572110fa4e0af216
fmohamed-a11y <f.mohamed@alustudent.com>
2026-08-01 08:47:34 -0700

    a script that displays the response body only for status 200 following redirects

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# a script that displays the body of a GET request only if status code is 200
-curl -s -o /tmp/body -w "%{http_code}" "$1" | grep -q "^200$" && cat /tmp/body
+# a script that displays the body of a GET request only if the final status code is 200, following redirects
+curl -s -L -o /tmp/body -w "%{http_code}" "$1" | grep -q "^200$" && cat /tmp/body
