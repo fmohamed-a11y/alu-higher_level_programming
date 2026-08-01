80ce64d385d9fc7b7d935affd9dd2632caf5a4fc
fmohamed-a11y <f.mohamed@alustudent.com>
2026-08-01 08:54:15 -0700

    correcting the output

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that sends a POST request with email and subject parameters and displays the response body
-curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
+curl -s -X POST --data-urlencode "email=test@gmail.com" --data-urlencode "subject=I will always be here for PLD" "$1"
