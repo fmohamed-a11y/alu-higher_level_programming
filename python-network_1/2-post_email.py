7d62724b52cad948365537b493440d2a88b95020
fmohamed-a11y <f.mohamed@alustudent.com>
2026-08-01 09:20:37 -0700

    a script that posts an email param and displays the response using urllib

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Send a POST request with an email parameter and display the response body."""
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
+    with urllib.request.urlopen(url, data=data) as response:
+        print(response.read().decode("utf-8"))
